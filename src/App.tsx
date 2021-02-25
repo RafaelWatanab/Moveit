f34b28c5bb69b8d42dcfd321d978feae03d7ead2
Rafael Watanabe <rafaelwatanabe2006@gmail.com>
2021-02-25 16:07:48 -0400

    in progress

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './style/global.css'
+
+import { Button } from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <Button color="red">
+        Botão 1
+      </Button>
+      <Button color="blue">
+        Botão 2
+      </Button>
+      <Button color="green">
+        Botão 3
+      </Button>
+    </div>
+
+  );
+}
+
+export default App;
